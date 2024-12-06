f6c7ba49c743cdd12ea08625fdbe147053b897a4
encoder-shoaib <ahammed15-4955@diu.edu.bd>
2024-12-07 01:32:55 +0600

    subtract total  amount  Donate for Flood at Feni

--- js/home.js
+++ js/home.js
@@ -9,6 +9,9 @@ document.getElementById('picture-1-button-1').addEventListener('click',function(
 // for picture 2 button 2
 document.getElementById('picture-2-button-2').addEventListener('click',function(){
     addDonateAmount ('picture-2-input-2','display-amaount-button-2')
+
+    subtractDonateAmount('picture-2-input-2','my-total-amount')
+
 })
 
 // for picture 3 button 3
