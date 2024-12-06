30b459a0e313e67d4c3924875b30ba79198c83ab
encoder-shoaib <ahammed15-4955@diu.edu.bd>
2024-12-07 01:30:14 +0600

    subtract for Flood at Noakhali

--- js/ShareFunction.js
+++ js/ShareFunction.js
@@ -25,6 +25,18 @@ function addDonateAmount (id1, id2){
     console.log(total)
     
     document.getElementById(id2).innerText = total;
+}
+
+// get 2 id and add there value then show  in id2
+function subtractDonateAmount (id1, id2){
+    const PictureInputValue = getInputFieldValueById(id1)
+    console.log(PictureInputValue);
+
+    const showAmount = getTextFieldValueById(id2)
+    console.log(showAmount);
+    const total = showAmount - PictureInputValue;
+    console.log(total)
     
+    document.getElementById(id2).innerText = total;
 }
 
--- js/home.js
+++ js/home.js
@@ -2,15 +2,16 @@
 document.getElementById('picture-1-button-1').addEventListener('click',function(){
     addDonateAmount ('picture-1-input-1','display-amaount-button-1')
 
+    subtractDonateAmount('picture-1-input-1','my-total-amount')
+
 })
 
-// for picture 1 button 1
+// for picture 2 button 2
 document.getElementById('picture-2-button-2').addEventListener('click',function(){
     addDonateAmount ('picture-2-input-2','display-amaount-button-2')
 })
 
-// for picture 1 button 1
+// for picture 3 button 3
 document.getElementById('picture-3-button-3').addEventListener('click',function(){
     addDonateAmount ('picture-3-input-3','display-amaount-button-3')
-
 })
