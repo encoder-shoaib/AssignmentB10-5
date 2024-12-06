789e3ef3b7ecd77debc6d221b90fe37f0983532f
encoder-shoaib <ahammed15-4955@diu.edu.bd>
2024-12-07 05:44:40 +0600

    add modal and all validation process complete

--- js/ShareFunction.js
+++ js/ShareFunction.js
@@ -14,17 +14,37 @@ function getTextFieldValueById(id){
 }
 
 // get 2 id and add there value then show  in id2
-function addDonateAmount (id1, id2){
+function addDonateAmount (id1, id2,id3){
     const PictureInputValue = getInputFieldValueById(id1)
     console.log(PictureInputValue);
 
     const showAmount = getTextFieldValueById(id2)
-    console.log(showAmount);
+    const myAmount = getTextFieldValueById(id3)
+
+    if (isNaN(PictureInputValue) || PictureInputValue < 0 || myAmount < PictureInputValue) {
+        const modal = document.getElementById("unSuccessModal");
+        modal.classList.remove("hidden"); // Show the modal
     
-    const total = showAmount + PictureInputValue;
-    console.log(total)
-    
-    document.getElementById(id2).innerText = total;
+        // event listener for the close button
+        document.getElementById("close-unsuccess-modal").addEventListener("click", function () {
+          modal.classList.add("hidden"); // Hide the modal
+        });
+        return;
+    }
+
+    else{
+        const total = showAmount + PictureInputValue;
+        document.getElementById(id2).innerText = total;
+        
+        const modal = document.getElementById("successModal");
+        modal.classList.remove("hidden"); // Show the modal
+        // Attach event listener to the close button
+        document.getElementById("close-success-modal").addEventListener("click", function () {
+              modal.classList.add("hidden"); // Hide the modal
+        });
+    }
+
+
 }
 
 // get 2 id and add there value then show  in id2
@@ -32,12 +52,33 @@ function subtractDonateAmount (id1, id2){
     const PictureInputValue = getInputFieldValueById(id1)
     console.log(PictureInputValue);
 
-    const showAmount = getTextFieldValueById(id2)
-    console.log(showAmount);
-    const total = showAmount - PictureInputValue;
-    console.log(total)
+    const myAmount = getTextFieldValueById(id2)
+    console.log(myAmount);
+
+
+    if (isNaN(PictureInputValue) || PictureInputValue < 0 || myAmount < PictureInputValue) {
+        const modal = document.getElementById("unSuccessModal");
+        modal.classList.remove("hidden"); // Show the modal
     
-    document.getElementById(id2).innerText = total;
+        // event listener for the close button
+        document.getElementById("close-unsuccess-modal").addEventListener("click", function () {
+          modal.classList.add("hidden"); // Hide the modal
+        });
+        return;
+    }
+
+    else{
+        const total = myAmount - PictureInputValue;
+        document.getElementById(id2).innerText = total;
+        
+        const modal = document.getElementById("successModal");
+        modal.classList.remove("hidden"); // Show the modal
+        // Attach event listener to the close button
+        document.getElementById("close-success-modal").addEventListener("click", function () {
+              modal.classList.add("hidden"); // Hide the modal
+        });
+    }
+
 }
 
 
--- js/home.js
+++ js/home.js
@@ -1,14 +1,15 @@
 // for picture 1 button 1
 document.getElementById('picture-1-button-1').addEventListener('click',function(){
-    addDonateAmount ('picture-1-input-1','display-amaount-button-1')
+    addDonateAmount ('picture-1-input-1','display-amaount-button-1','my-total-amount');
 
     subtractDonateAmount('picture-1-input-1','my-total-amount')
+
 
 })
 
 // for picture 2 button 2
 document.getElementById('picture-2-button-2').addEventListener('click',function(){
-    addDonateAmount ('picture-2-input-2','display-amaount-button-2')
+    addDonateAmount ('picture-2-input-2','display-amaount-button-2','my-total-amount')
 
     subtractDonateAmount('picture-2-input-2','my-total-amount')
 
@@ -16,16 +17,29 @@ document.getElementById('picture-2-button-2').addEventListener('click',function(
 
 // for picture 3 button 3
 document.getElementById('picture-3-button-3').addEventListener('click',function(){
-    addDonateAmount ('picture-3-input-3','display-amaount-button-3')
+    addDonateAmount ('picture-3-input-3','display-amaount-button-3','my-total-amount')
 
     subtractDonateAmount('picture-3-input-3','my-total-amount')
 })
 
 
-
+// for history button work 
 document.getElementById('history-btn').addEventListener('click',function(){
-    showSectionById('history-btn')
+        // add hidden of all section 
+        document.getElementById('donate-section').classList.add('hidden')
+        // remove hidden by id 
+        document.getElementById('hide-section').classList.remove('hidden');
 })
+
+// for Donation button work 
+document.getElementById('donation-btn').addEventListener('click',function(){
+        // add hidden of all section 
+        document.getElementById('hide-section').classList.add('hidden')
+        // remove hidden by id 
+        document.getElementById('donate-section').classList.remove('hidden');
+})
+
+
 
 
 
